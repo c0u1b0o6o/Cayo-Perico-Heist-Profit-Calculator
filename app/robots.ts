c052f13a30eae1e4af461a798bdbfd83476b2c84
cuboo877 <cuboomax@gmail.com>
2026-02-28 12:37:41 +0800

    修正SEO優化，robots.ts與sitemap.ts已build

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,14 @@
-export default function robots() {
+import { MetadataRoute } from 'next';
+
+// 告訴 Next.js 在 build 的時候把這份檔案直接轉成靜態的 .txt
+export const dynamic = 'force-static';
+
+export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
     sitemap: 'https://cayo-perico.cuboouo.com/sitemap.xml',
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,11 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+  return[
     {
-      url: 'https://cayo-perico.cuboouo.com/',
+      url: 'https://cayo-perico.cuboouo.com',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
